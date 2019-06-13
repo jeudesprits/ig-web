898e4cc15850037013e29811f9f13d3cabf1d374
jeudesprits <jeudesprits@icloud.com>
2019-06-13 21:51:56 +0500

    Add some deps.

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,14 @@
 console.log('Hello, muchachos!');
+
+function isValidJSON(text: string): boolean {
+  try {
+    JSON.parse(text);
+    return true;
+  } catch {
+    return false;
+  }
+}
+
+for (const item of [1, 2, 3]) {
+  console.log(item);
+}
