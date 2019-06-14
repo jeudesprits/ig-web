6e95404a329f99997a54f60ae80b0e3a71be513a
jeudesprits <jeudesprits@icloud.com>
2019-06-14 14:36:01 +0500

    Change return on 'launch' method in 'Browser' class

--- src/browser/index.ts
+++ src/browser/index.ts
@@ -5,7 +5,7 @@ export default class Browser {
   static browser: puppeteer.Browser;
 
   static async launch() {
-    return new Promise<puppeteer.Browser>((res, rej) => {
+    return new Promise<void>((res, rej) => {
       puppeteer.launch({
         args: [
           '--user-agent=Mozilla/5.0 (iPhone; CPU iPhone OS 12_3_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/12.1.1 Mobile/15E148 Safari/604.1',
@@ -22,7 +22,7 @@ export default class Browser {
         },
       }).then(val => {
         this.browser = val;
-        res(this.browser);
+        res();
       }).catch(e => {
         rej(e);
       })
