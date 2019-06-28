65836dae39a9202078b8cae1b4e88defb58cd253
jeudesprits <jeudesprits@icloud.com>
2019-06-28 12:07:59 +0000

    Add VSCode dev container

--- src/server.ts
+++ src/server.ts
@@ -7,5 +7,6 @@ import IGApi from './api/ig/login';
   const api = new IGApi();
   await api.prepare();
   await api.logIn('jeudesprits', 'rj2119942104sl');
+  console.log(await api.feedReels());
   await Browser.close();
 })();
