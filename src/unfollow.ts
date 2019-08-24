40991ab14d17ce9b96798d8a8ef8d4208b6a74d6
Ruslan Lutfullin <jeudesprits@icloud.com>
2019-08-24 20:30:12 +0000

    Unfollow user change

--- src/unfollow.ts
+++ src/unfollow.ts
@@ -6,8 +6,7 @@ const { G_USERNAME, G_PASSWORD } = secrets;
 import { msleep } from './utils/helpers';
 import cron from 'node-cron';
 
-// tslint:disable-next-line: no-floating-promises
-(async () => {
+cron.schedule('*/15 * * * *', async () => {
     const browser = new Browser();
     await browser.launch();
 
@@ -16,35 +15,30 @@ import cron from 'node-cron';
     await igApi.Result;
     await igApi.logIn(G_USERNAME, G_PASSWORD);
 
-    cron.schedule('*/15 * * * *', async () => {
-        try {
-            let count = 0;
-            loop: for await (const data of igApi.profileFollowing(G_USERNAME)) {
-                // prettier-ignore
-                const { data: { user: { edge_follow: { edges } } } } = data;
+    try {
+        let count = 0;
+        loop: for await (const data of igApi.profileFollowing(G_USERNAME)) {
+            // prettier-ignore
+            const { data: { user: { edge_follow: { edges } } } } = data;
 
-                // prettier-ignore
-                for (const { node: { username } } of edges) {
-                    if (count >= 15) { break loop; }
+            // prettier-ignore
+            for (const { node: { username } } of edges) {
+                if (count >= 15) { break loop; }
 
-                    ++count;
-                    try {
-                        await igApi.profileUnfollow(username);
-                    } catch (error) {
-                        await browser.screenshot(igApi.sessionPage, './tmp/screenshot.jpg');
-                        logger.error(error.stack, { label: `ig-web @${G_USERNAME}` });
-                    }
-                    await msleep(2000);
+                ++count;
+                try {
+                    await igApi.profileUnfollow(username);
+                } catch (error) {
+                    await browser.screenshot(igApi.sessionPage, './tmp/screenshot.jpg');
+                    logger.error(error.stack, { label: `ig-web @${G_USERNAME}` });
                 }
+                await msleep(2000);
             }
-        } catch (error) {
-            await browser.screenshot(igApi.sessionPage, './tmp/screenshot.jpg');
-            logger.error(error.stack, { label: `ig-web @${G_USERNAME}` });
-        } finally {
-            (await browser.pages()).forEach((value, _index, _array) => {
-                // tslint:disable-next-line: no-floating-promises
-                value.close();
-            });
         }
-    });
-})();
+    } catch (error) {
+        await browser.screenshot(igApi.sessionPage, './tmp/screenshot.jpg');
+        logger.error(error.stack, { label: `ig-web @${G_USERNAME}` });
+    } finally {
+        await browser.close();
+    }
+});
